1fdbc4b1061f1a8ad8dd3bf28af9ab81267183b0
ghostxsl <451323469@qq.com>
2023-12-26 13:49:57 +0800

    [Animate] update stage_1 infer code

--- animate_infer_stage_1.py
+++ animate_infer_stage_1.py
@@ -1,5 +1,5 @@
 import os
-from os.path import join, splitext
+from os.path import join
 from collections import OrderedDict
 import random
 import numpy as np
@@ -7,13 +7,12 @@ from PIL import Image, ImageOps
 import pandas
 import torch
 
-from transformers import AutoProcessor, CLIPVisionModel
 from diffusers.pipelines.vip.vip_animate_image import VIPAnimateImagePipeline
 from diffusers.schedulers import EulerAncestralDiscreteScheduler
 from diffusers.models import ControlNetModel
 from diffusers.models.referencenet import ReferenceNetModel
-from diffusers.models.pose_guider import PoseGuider
 from diffusers.utils.vip_utils import *
+
 
 device = torch.device("cuda")
 dtype = torch.float16
@@ -21,14 +20,15 @@ stride = 4
 num_frames = 24
 
 root_dir = "/xsl/wilson.xu/fashion_video"
-out_dir = "output"
+
 csv_file = join(root_dir, "train_png.csv")
 img_dir = join(root_dir, "train_png")
 pose_dir = join(root_dir, "train_png_pose")
-base_path = "/xsl/wilson.xu/weights/stable-diffusion-v1-5"
-pose_guider_path = "/xsl/wilson.xu/animate_sd1.5_ctrl_10/pose_guider"
-referencenet_model_path = "/xsl/wilson.xu/animate_sd1.5_ctrl_10/referencenet"
-controlnet_path = "/xsl/wilson.xu/animate_sd1.5_ctrl_10/controlnet"
+
+base_path = "/xsl/wilson.xu/weights/film"
+referencenet_model_path = "/xsl/wilson.xu/animate_film_ctrl_100/referencenet"
+controlnet_path = "/xsl/wilson.xu/animate_film_ctrl_100/controlnet"
+out_dir = "output"
 os.makedirs(out_dir, exist_ok=True)
 
 
@@ -103,30 +103,24 @@ if __name__ == '__main__':
         else:
             video_to_image[video_name].append(name)
 
-    feature_extractor = AutoProcessor.from_pretrained(join(base_path, "image_encoder"))
-    image_encoder = CLIPVisionModel.from_pretrained(base_path, subfolder="image_encoder").to(device, dtype=dtype)
     controlnet = ControlNetModel.from_pretrained(controlnet_path).to(device, dtype=dtype)
-    # pose_guider = PoseGuider.from_pretrained(pose_guider_path).to(device, dtype=dtype)
     referencenet = ReferenceNetModel.from_pretrained(referencenet_model_path).to(device, dtype=dtype)
     pipe = VIPAnimateImagePipeline.from_pretrained(
         base_path,
-        feature_extractor=feature_extractor,
-        image_encoder=image_encoder,
         controlnet=controlnet,
-        # pose_guider=pose_guider,
         referencenet=referencenet,
         torch_dtype=dtype).to(device)
     pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 
-    seed = get_fixed_seed(-1)
-    generator = get_torch_generator(seed, device=device)
-
+    ind = 1
     for k, v in video_to_image.items():
-        print(k)
+        print(f"{ind}: {k}")
+        seed = get_fixed_seed(-1)
+        generator = get_torch_generator(seed, device=device)
 
         reference, pose, gt_img = get_reference_pose_frame(v)
         out = pipe(
-            prompt="",
+            prompt="woman posing for a photo, simple white background",
             reference_image=reference,
             control_image=pose,
             height=512,
@@ -141,5 +135,6 @@ if __name__ == '__main__':
             [reference.resize((512, 512), 1), pose.resize((512, 512), 1),
              gt_img.resize((512, 512), 1), out[0]], axis=1)
         Image.fromarray(out_img).save(join(out_dir, k + '.png'))
+        ind += 1
 
     print('Done!')
