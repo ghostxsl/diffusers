e82a9eec68a22dfda7bc7ca85fd889e5d117f75f
ghostxsl <451323469@qq.com>
2024-01-10 14:01:59 +0800

    update animate dataset to use pose points

--- examples/vip/train_animate_anyone_stage_1.py
+++ examples/vip/train_animate_anyone_stage_1.py
@@ -44,7 +44,7 @@ from diffusers import (
 )
 from diffusers.optimization import get_scheduler
 from diffusers.utils.import_utils import is_xformers_available
-from diffusers.data import AnimateAnyoneDataset, animate_collate_fn
+from diffusers.data import AnimateDataset, animate_collate_fn
 from diffusers.models.reference_attention import ReferenceAttentionControl
 from diffusers.models.referencenet import ReferenceNetModel
 from diffusers.models.pose_guider import PoseGuider
@@ -561,7 +561,7 @@ def main(args):
     )
 
     # Dataset and DataLoaders creation:
-    train_dataset = AnimateAnyoneDataset(
+    train_dataset = AnimateDataset(
         dataset_csv=args.dataset_csv,
         train_data_dir=args.train_data_dir,
         condition_data_dir=args.condition_data_dir,
